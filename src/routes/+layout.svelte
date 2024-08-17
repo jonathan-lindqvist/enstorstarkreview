769a97c2aa9eb4c00f7af506db5c468b250e5206
Mike Johansson <mike.johansson.mj@gmail.com>
2024-08-17 19:28:55 +0200

    Remove skeleton files

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -9,10 +9,6 @@
 	<main>
 		<slot />
 	</main>
-
-	<footer>
-		<p>visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to learn SvelteKit</p>
-	</footer>
 </div>
 
 <style>
@@ -22,23 +18,5 @@
 		height: 100%;
 		width: 100%;
 		padding: 0 24px;
-	}
-
-	footer {
-		display: flex;
-		flex-direction: column;
-		justify-content: center;
-		align-items: center;
-		padding: 12px;
-	}
-
-	footer a {
-		font-weight: bold;
-	}
-
-	@media (min-width: 480px) {
-		footer {
-			padding: 12px 0;
-		}
 	}
 </style>
