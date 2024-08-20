a6883d96fd26d4567b659a87d0bde06ea5c4558c
Jonathan Lindqvist <jonathan.x.lindqvist@gmail.com>
2024-08-20 21:08:42 +0200

    Add from to submit bar reviews

---
+++ src/routes/review/+page.server.ts
@@ -0,0 +1,65 @@
+import { fail, redirect } from '@sveltejs/kit';
+import type { Actions, PageServerLoad } from './$types';
+import { bars } from '$lib/db/bars';
+import type { BarReview } from '$lib/types/bar-review';
+
+export const load: PageServerLoad = async ({ locals }) => {
+	console.log(locals.user);
+	if (!locals.user) redirect(302, '/login');
+
+	return null;
+};
+
+export const actions: Actions = {
+	review: async ({ request }) => {
+		const formData = await request.formData();
+		const title = formData.get('title');
+		const description = formData.get('description');
+		const rating = Number(formData.get('rating'));
+		const imagePath = formData.get('imagePath');
+		const location = formData.get('location');
+		const slug = formData.get('slug');
+
+		if (typeof title !== 'string' || title.length > 255 || title.length < 2) {
+			return fail(400, { message: 'Invalid bar title' });
+		}
+		if (typeof description !== 'string' || description.length < 2) {
+			return fail(400, { message: 'Invalid bar description' });
+		}
+
+		if (isNaN(rating) || rating < 0 || rating > 3) {
+			return fail(400, { message: 'Invalid bar rating' });
+		}
+
+		if (typeof imagePath !== 'string' || imagePath.length < 5) {
+			return fail(400, { message: 'Invalid bar imagePath' });
+		}
+
+		if (typeof location !== 'string' || location.length < 5) {
+			return fail(400, { message: 'Invalid bar location' });
+		}
+
+		if (typeof slug !== 'string' || slug.length < 2 || !/^[a-z0-9-]+$/.test(slug)) {
+			return fail(400, { message: 'Invalid bar slug' });
+		}
+
+		const barDocument: BarReview = {
+			title: title,
+			description: description,
+			rating: rating,
+			image: imagePath,
+			location: location,
+			slug: slug,
+			createdAt: new Date(),
+			updatedAt: new Date()
+		};
+
+		const result = await bars.insertOne(barDocument);
+
+		if (!result.acknowledged || !result.insertedId) {
+			return fail(400, { message: 'Could not save review' });
+		}
+
+		return { status: 200, body: { message: 'Review saved!' } };
+	}
+};
---
+++ src/routes/review/constants.ts
@@ -0,0 +1,32 @@
+export const descriptionTemplate = `=== Helhetsbild (ta bort rubrik) ===
+
+=== Öl kvalite ===
+Smak: Hur är ölets smakprofil? Är den frisk, välbalanserad, eller känns den platt?
+
+Kvalitet: Känns ölet färskt? Är det välkolsyrat eller smaklöst?
+
+Serveringstemperatur: Serverades ölen vid rätt temperatur? Är den för varm eller för kall?
+
+=== Prisvärdhet ===
+Pris: Hur mycket kostade ölen? Var det ett rimligt pris för en "stor stark" på denna bar?
+
+Pris vs. Kvalitet: Får man vad man betalar för? Är ölet värt sitt pris?
+
+=== Servering och Presentation ===
+Glas: Vilket glas serverades ölen i? Var det rent och passande för ölsorten?
+
+Skumkrona: Hur var skumkronan? Var den stabil eller försvann den snabbt?
+
+Mängd: Var glaset välfyllt, eller saknades det öl i toppen?
+
+=== Atmosfär ===
+Miljö: Hur är stämningen på baren? Är det en trevlig miljö att dricka en öl i?
+
+Service: Hur var servicen? Var personalen trevlig och kunnig om ölen?
+
+Ljudnivå: Var det en behaglig ljudnivå, eller var det för högljutt/för tyst?
+
+=== Kommentarer ===
+Positivt: Vad gjorde baren bra?
+
+Negativt: Vad kan förbättras?`;
