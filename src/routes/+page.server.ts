8aabe91df5c3e844874444ccf4a27794571ed7c8
Jonathan Lindqvist <jonathan.x.lindqvist@gmail.com>
2024-08-22 20:22:17 +0200

    Add regex search for bars

--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -1,8 +1,16 @@
 import { bars } from '$lib/db/bars';
 import type { PageServerLoad } from './$types';
 
-export const load: PageServerLoad = async function () {
-	const data = await bars.find().toArray();
+export const load: PageServerLoad = async ({ url }) => {
+	const searchQuery = url.searchParams.get('query')?.toLowerCase() || '';
+
+	const data = await bars.find({
+		$or: [
+			{ title: { $regex: searchQuery, $options: 'i' } },
+			{ description: { $regex: searchQuery, $options: 'i' } },
+			{ location: { $regex: searchQuery, $options: 'i' } }
+		]
+	}).toArray();
 
 	const serializedData = data.map((item) => ({
 		...item,
