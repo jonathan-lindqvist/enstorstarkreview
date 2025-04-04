0c36e2805e8741d3f3520da4ac0399443012d5c5
Mike Johansson <mike.johansson.mj@gmail.com>
2025-04-04 14:54:00 +0200

    Rework Searchbar to be in Svelte 4 and Tailwind 4

---
+++ src/lib/components/SearchBar.svelte
@@ -0,0 +1,19 @@
+<script lang="ts">
+	import SearchIcon from '$lib/components/svgs/SearchIcon.svelte';
+</script>
+
+<form class="flex w-fit items-center h-10 gap-2" method="get">
+	<input
+		name="search"
+		id="search"
+		type="text"
+		class="bg-white h-full rounded px-2 focus:outline-none focus:ring-orange-600 focus:ring-2"
+	/>
+	<button
+		type="submit"
+		class="flex gap-1 bg-orange-600 px-4 rounded h-full items-center hover:cursor-pointer"
+	>
+		<SearchIcon />
+		Sök
+	</button>
+</form>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,7 +1,6 @@
 <script lang="ts">
 	import Card from '$lib/components/Card.svelte';
-	import type { PageData } from './$types';
-	import SearchBar from './SearchBar.svelte';
+	import SearchBar from '$lib/components/SearchBar.svelte';
 
 	let { data } = $props();
 </script>
@@ -11,10 +10,10 @@
 	<meta name="description" content="En stor stark review" />
 </svelte:head>
 
-<section class="h-full">
+<section class="h-full bg-primary-gray px-6 py-6">
 	<SearchBar />
 	{#each data.bars as bar}
-		<a href={bar.slug} class="hover:no-underline">
+		<a href={bar.slug} class="hover:no-underline block mt-4" >
 			<Card
 				title={bar.title}
 				description={bar.description}
