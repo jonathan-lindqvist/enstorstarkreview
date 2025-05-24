05ae6047e8e51b4b265a8f953c75078d0b206a59
Mike Johansson <mike.johansson.mj@gmail.com>
2025-05-24 15:18:53 +0200

    Update reviews list to show the image of the bar

--- src/routes/admin/reviews/+page.svelte
+++ src/routes/admin/reviews/+page.svelte
@@ -11,17 +11,19 @@
 	<div class="max-w-[500px] mx-auto">
 		<div class="flex justify-between items-center">
 			<h1 class="text-2xl underline">Reviews</h1>
-			<a href="/admin/review/create" class="text-white bg-orange-600 py-2 px-6 rounded">Create</a>
+			<a href="/admin/reviews/create" class="text-white bg-orange-600 py-2 px-6 rounded">Create</a>
 		</div>
 		<ul class="space-y-4 mt-4">
 			{#each data.bars as bar}
 				<li class="flex bg-gray-800 py-2 px-6 rounded-lg items-center gap-4">
-					<img src={bar.image ?? defaultImage} alt={bar.title} class="rounded-full h-20">
+					<div class="h-20 w-20">
+						<img src={bar.image ? `/images/${bar.image}` : undefined} alt={bar.title} class="h-full w-full object-contain">
+					</div>
 					<div>
 						<h2 class="text-lg">{bar.title}</h2>
 					</div>
 					<div class="ml-auto">
-						<a href="/admin/review/edit/{bar.slug}" class="text-white bg-orange-600 py-2 px-6 rounded block">Edit</a>
+						<a href="/admin/reviews/edit/{bar.slug}" class="text-white bg-orange-600 py-2 px-6 rounded block">Edit</a>
 					</div>
 				</li>
 			{/each}
