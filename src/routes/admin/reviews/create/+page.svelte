02dc6618fcbcce368e69866348e70568a33456ea
Jonathan Lindqvist <jonathan.x.lindqvist@gmail.com>
2026-02-28 19:09:41 +0100

    Remove success case because unreachable

--- src/routes/admin/reviews/create/+page.svelte
+++ src/routes/admin/reviews/create/+page.svelte
@@ -28,7 +28,7 @@
 
 	{#if form?.message}
 		<div
-			class={`mt-6 rounded-2xl border p-4 text-sm ${form.pointer ? 'border-red-400/60 bg-red-100 text-red-700' : 'border-emerald-400/60 bg-emerald-100 text-emerald-700'}`}
+			class="mt-6 rounded-2xl border border-red-400/60 bg-red-100 p-4 text-sm text-red-700"
 		>
 			{form.message}
 		</div>
